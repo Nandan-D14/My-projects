0a887bec4ac958555d04f29325abab2047ee5a6c
Nandan-D14 <154668653+Nandan-D14@users.noreply.github.com>
2024-10-04 23:05:47 +0530

    Add files via upload

---
+++ typingGame.js
@@ -0,0 +1,246 @@
+
+
+const texts = [
+    "Their politician was, in this moment, a notour paperback. The first armless grouse is, in its own way, a gear. The coat is a wash. However, a cake is the llama of a caravan. Snakelike armies show us how playgrounds can be viscoses. Framed in a different way, they were lost without the fatal dogsled that composed their waitress. Far from the truth, the cockney freezer reveals itself as a wiggly tornado to those who look. The first hawklike sack.",
+    "Authors often misinterpret the lettuce as a folklore rabbi, when in actuality it feels more like an uncursed bacon. Pursued distances show us how mother-in-laws can be charleses. Authors often misinterpret the lion as a cormous science, when in actuality it feels more like a leprous lasagna. Recent controversy aside, their band was, in this moment, a racemed suit. The clutch of a joke becomes a togaed chair. The first pickled chess is.",
+    "In modern times the first scrawny kitten is, in its own way, an input. An ostrich is the beginner of a roast. An appressed exhaust is a gun of the mind. A recorder is a grade from the right perspective. A hygienic is the cowbell of a skin. Few can name a dun brazil that isn't a highbrow playroom. The unwished beast comes from a thorny oxygen. An insured advantage's respect comes with it the thought that the lucid specialist is a fix.",
+    "What we don't know for sure is whether or not a pig of the coast is assumed to be a hardback pilot. The literature would have us believe that a dusky clave is not but an objective. Few can name a limbate leo that isn't a sunlit silver. The bow is a mitten. However, the drawer is a bay. If this was somewhat unclear, few can name a paunchy blue that isn't a conoid bow. The undrunk railway reveals itself as a downstage bamboo to those who look.",
+    "An aunt is a bassoon from the right perspective. As far as we can estimate, some posit the melic myanmar to be less than kutcha. One cannot separate foods from blowzy bows. The scampish closet reveals itself as a sclerous llama to those who look. A hip is the skirt of a peak. Some hempy laundries are thought of simply as orchids. A gum is a trumpet from the right perspective. A freebie flight is a wrench of the mind. Some posit the croupy.",
+    "A baby is a shingle from the right perspective. Before defenses, collars were only operations. Bails are gleesome relatives. An alloy is a streetcar's debt. A fighter of the scarecrow is assumed to be a leisured laundry. A stamp can hardly be considered a peddling payment without also being a crocodile. A skill is a meteorology's fan. Their scent was, in this moment, a hidden feeling. The competitor of a bacon becomes a boxlike cougar.",
+    "A broadband jam is a network of the mind. One cannot separate chickens from glowing periods. A production is a faucet from the right perspective. The lines could be said to resemble zincoid females. A deborah is a tractor's whale. Cod are elite japans. Some posit the wiglike norwegian to be less than plashy. A pennoned windchime's burst comes with it the thought that the printed trombone is a supply. Relations are restless tests.",
+    "In recent years, some teeming herons are thought of simply as numbers. Nowhere is it disputed that an unlaid fur is a marble of the mind. Far from the truth, few can name a glossy lier that isn't an ingrate bone. The chicken is a giraffe. They were lost without the abscessed leek that composed their fowl. An interviewer is a tussal bomb. Vanward maracas show us how scarfs can be doubts. Few can name an unguled punch that isn't pig.",
+    "A cough is a talk from the right perspective. A designed tractor's tray comes with it the thought that the snuffly flax is a rainbow. Their health was, in this moment, an earthy passbook. This could be, or perhaps the swordfishes could be said to resemble healthy sessions. A capricorn is a helium from the right perspective. However, a sled is a mailman's tennis. The competitor of an alarm becomes a toeless raincoat. Their twist was, in this moment.",
+    "Authors often misinterpret the flag as a wayless trigonometry, when in actuality it feels more like a bousy gold. Few can name a jasp oven that isn't a stutter grape. They were lost without the huffy religion that composed their booklet. Those waves are nothing more than pedestrians. Few can name a quartered semicolon that isn't a rounding scooter. Though we assume the latter, the literature would have us believe.",
+    "This could be, or perhaps few can name a pasteboard quiver that isn't a brittle alligator. A swordfish is a death's numeric. Authors often misinterpret the mist as a swelling asphalt, when in actuality it feels more like a crosswise closet. Some posit the tonal brother-in-law to be less than newborn. We know that the sizes could be said to resemble sleepwalk cycles. Before seasons, supplies were only fighters. Their stew was, in this moment.",
+    "The vision of an attempt becomes a lawny output. Dibbles are mis womens. The olden penalty reveals itself as a bustled field to those who look. Few can name a chalky force that isn't a primate literature. However, they were lost without the gamy screen that composed their beret. Nowhere is it disputed that a step-uncle is a factory from the right perspective. One cannot separate paints from dreary windows. What we don't know for sure is whether.",
+    "A tramp is a siamese from the right perspective. We know that a flitting monkey's jaw comes with it the thought that the submersed break is a pamphlet. Their cream was, in this moment, a seedy daffodil. The nest is a visitor. Far from the truth, they were lost without the released linen that composed their step-sister. A vibraphone can hardly be considered a pardine process without also being an archaeology. The bay of a hyacinth becomes.",
+    "The frosts could be said to resemble backstage chards. One cannot separate colleges from pinkish bacons. Far from the truth, the mom of a rooster becomes a chordal hydrogen. A tempo can hardly be considered a purer credit without also being a pajama. The first combined ease is, in its own way, a pantyhose. Extending this logic, the guides could be said to resemble reddest monkeies. Framed in a different way, an addle hemp is a van.",
+    "Far from the truth, an ajar reminder without catamarans is truly a foundation of smarmy semicircles. An alike board without harps is truly a satin of fated pans. A hubcap sees a parent as a painful beautician. The zeitgeist contends that some intense twigs are thought of simply as effects. A cross is a poppied tune. The valanced list reveals itself as an exchanged wrist to those who look. Recent controversy aside.",
+    "The hefty opinion reveals itself as a sterile peer-to-peer to those who look. This could be, or perhaps the watch of a diamond becomes a bosom baboon. In recent years, some posit the unstuffed road to be less than altern. It's an undeniable fact, really; the livelong lettuce reveals itself as an unstuffed soda to those who look. In ancient times a bit is a balance's season. The popcorn of a morning becomes a moonless beauty.",
+    "If this was somewhat unclear, a friend is a fridge from the right perspective. An upset carriage is a stitch of the mind. To be more specific, a temper is a pair from the right perspective. Authors often misinterpret the liquid as a notchy baseball, when in actuality it feels more like an unbarbed angle. Though we assume the latter, the first vagrom report is, in its own way, a tower. We know that the octopus of a cd becomes an unrent dahlia.",
+    "A reptant discussion's rest comes with it the thought that the condemned syrup is a wish. The drake of a wallaby becomes a sonant harp. If this was somewhat unclear, spotty children show us how technicians can be jumps. Their honey was, in this moment, an intime direction. A ship is the lion of a hate. They were lost without the croupous jeep that composed their lily. In modern times a butcher of the birth is assumed to be a spiral bean.",
+    "Those cowbells are nothing more than elements. This could be, or perhaps before stockings, thoughts were only opinions. A coil of the exclamation is assumed to be a hurtless toy. A board is the cast of a religion. In ancient times the first stinko sailboat is, in its own way, an exchange. Few can name a tutti channel that isn't a footless operation. Extending this logic, an oatmeal is the rooster of a shake. Those step-sons are nothing more than matches.",
+     "In the heart of the bustling city, a sense of tranquility can be found. Amidst the towering skyscrapers and bustling streets, there exists a hidden oasis of calm. Here, time seems to slow down, allowing one to pause and reflect amidst the chaos of urban life. The gentle rustling of leaves in the breeze and the distant hum of city life create a harmonious melody that soothes the soul. This juxtaposition of serenity and dynamism defines the essence of city living—a delicate balance between peace and progress.",
+    "As dawn breaks over the horizon, a new day unfolds with promises of hope and opportunity. The sun's golden rays pierce through the morning mist, casting a warm glow upon the dew-kissed meadows. Birds chirp joyfully in the awakening silence, heralding the arrival of another day filled with endless possibilities. Nature's canvas paints a picture of renewal and vitality, reminding us of the beauty that lies in every sunrise. With each passing moment, the world awakens to embrace the gift of a brand-new day.",
+    "Lost in the labyrinthine corridors of the ancient castle, whispers of forgotten tales echo through the stone walls. Shadows dance ominously as flickering torches cast eerie shadows upon the worn tapestries and weathered armor. Centuries-old secrets are hidden within these hallowed halls, waiting to be unearthed by those brave enough to tread the paths of history. Each creaking floorboard and gust of wind tells a tale of valor and intrigue, weaving a tapestry of legends that have stood the test of time.",
+];
+
+const selectedTime = document.getElementById("time-select");
+const wordDisplay = document.getElementById("word-display");
+const inputArea = document.getElementById("input-area");
+const timeEle = document.getElementById("time");
+const accuracyEle = document.getElementById("accuracy");
+const wpmEle = document.getElementById("wpm");
+const mistakesEle = document.getElementById("Mistakes");
+
+let timeLeft = parseInt(selectedTime.value);
+let Accuracy = 0;
+let wpm = 0;
+let timerInterval;
+let timerInterval2;
+let totalWordsTyped = 0;
+let correctChar = 0;
+let totalCharTyped = 0;
+let gameStarted = false;
+let count = 0;
+let lenOfPara = 0;
+let paragraph = "";
+let mistakes = 0;
+let timeConsume = 0;
+let data = {
+    dwpm: 0,
+    daccuracy: 0,
+    dmistakes: 0,
+    dtime: 0,
+    dcorrectCount: 0,
+    dlenofpara: 0,
+    dtimeconsume: 0
+};
+
+timeEle.innerText = `Time: ${timeLeft}s`;
+wpmEle.innerText = `WPM: ${wpm}`;
+accuracyEle.innerText = `Accuracy: ${Accuracy}%`;
+mistakesEle.innerText = `Mistakes: ${mistakes}`;
+
+function loadePara() {
+    let randomText = Math.floor(Math.random() * texts.length);
+    wordDisplay.innerText = '';
+    paragraph = '';
+    lenOfPara = 0;
+    texts[randomText].split('').forEach((char) => {
+        paragraph += char;
+        let span = `<span id="textSpan${lenOfPara}">${char}</span>`;
+        wordDisplay.innerHTML += span;
+        lenOfPara++;
+    });
+    data.dlenofpara = lenOfPara;
+    document.addEventListener("keydown", () => inputArea.focus());
+    inputArea.addEventListener("click", () => inputArea.focus());
+}
+
+function calculateWPM() {
+    const wordsTyped = inputArea.value.trim().split(/\s+/).filter(word => word.length > 0).length;
+    const minutes = (selectedTime.value - timeLeft) / 60;
+
+    if (minutes > 0) {
+        return Math.floor(wordsTyped / minutes);
+    } else {
+        return 0;
+    }
+}
+
+function calculateAccuracy() {
+    let inputLetters = inputArea.value.split('');
+    let targetText = paragraph.split('');
+    let correctCount = 0;
+
+    inputLetters.forEach((char, index) => {
+        if (char === targetText[index]) {
+            correctCount++;
+        }
+    });
+    if (inputLetters.length === 0) {
+        return 100;
+    }
+    return Math.floor((correctCount / inputLetters.length) * 100);
+}
+
+function startTimer() {
+    timeLeft = parseInt(selectedTime.value); // Get the selected time
+    timerInterval = setInterval(() => {
+        timeLeft--;
+        timeEle.innerText = `Time: ${timeLeft}s`;
+        if (timeLeft === 0) {
+            clearInterval(timerInterval);
+            inputArea.disabled = true;
+            resultDisplay();
+        }
+    }, 1000);
+}
+
+function TotalTimeConsume() {
+    timerInterval2 = setInterval(() => {
+        timeConsume++;
+        if (timeLeft === timeConsume) {
+            clearInterval(timerInterval2);
+        }
+    }, 1000);
+    return timeConsume;
+}
+
+function checkInput() {
+    let textSpan = document.getElementById(`textSpan${count}`).innerText;
+    let inputText = inputArea.value.split("")[count];
+    data.dtimeconsume = TotalTimeConsume();
+    if (correctChar === lenOfPara) {
+        resultDisplay();
+        clearInterval(timerInterval); 
+        clearInterval(timerInterval2); 
+    }
+    if (inputText != null) {
+        if (inputText === textSpan) {
+            document.getElementById(`textSpan${count}`).style.color = "lightgreen";
+            correctChar++;
+        } else {
+            document.getElementById(`textSpan${count}`).style.color = "rgb(249, 76, 76) ";
+            mistakes++;
+        }
+    }
+
+    if (inputArea.value.length < count) {
+        count--;
+        if (document.getElementById(`textSpan${count}`).style.color === "red") {
+            mistakes--;
+        }
+        document.getElementById(`textSpan${count}`).style.color = "rgba(241, 239, 239, 0.276)";
+    } else {
+        count++;
+    }
+    wpm = calculateWPM();
+    data.dwpm = wpm;
+    Accuracy = calculateAccuracy();
+    data.daccuracy = Accuracy;
+    data.dmistakes = mistakes;
+    data.dcorrectCount = correctChar;
+    wpmEle.innerText = `WPM: ${wpm}`;
+    accuracyEle.innerText = `Accuracy: ${Accuracy}%`;
+    mistakesEle.innerText = `Mistakes: ${mistakes}`;
+}
+
+function resetGame() {
+    clearInterval(timerInterval); 
+    clearInterval(timerInterval2);
+    timeLeft = parseInt(selectedTime.value); 
+    mistakes = 0;
+    Accuracy = 0;
+    wpm = 0;
+    correctChar = 0;
+    gameStarted = false;
+    inputArea.value = "";
+    inputArea.disabled = false;
+    count = 0;
+    lenOfPara = 0;
+    paragraph = "";
+    timeEle.innerText = `Time: ${timeLeft}s`;
+    wpmEle.innerText = `WPM: ${wpm}`;
+    accuracyEle.innerText = `Accuracy: ${Accuracy}%`;
+    mistakesEle.innerText = `Mistakes: ${mistakes}`;
+    wordDisplay.innerHTML = "";
+    loadePara();
+    resultDisplayRemove();
+}
+const result = document.createElement('div');
+result.classList.add("result");
+
+function resultDisplay() {
+    result.innerHTML = `<h3> Keep trying !</h3>
+                    <pre>WPM                   ${data.dwpm}</pre>
+                    <pre>Accuracy              ${data.daccuracy}%</pre>
+                    <pre>Mistakes              ${data.dmistakes}</pre>
+                    <pre>Corrects              ${data.dcorrectCount}</pre>
+                    <pre>Total words           ${data.dlenofpara}</pre>
+                    `;
+                    
+    document.getElementsByTagName("h1")[0].after(result);
+    
+    const resultElementX = document.getElementsByClassName("result")[0];
+    resultElementX.addEventListener("click", () => {
+        resultElementX.remove("result"); 
+    });
+}
+
+function resultDisplayRemove() {
+    const resultElement = document.getElementsByClassName("result")[0]; 
+    if (resultElement) {
+        resultElement.remove(); 
+    }
+}
+
+loadePara();
+document.getElementById('input-area').setAttribute('autocomplete', 'off');
+
+inputArea.addEventListener("input", () => {
+    if (!gameStarted) {
+        gameStarted = true;
+        startTimer();
+        checkInput();
+    } else {
+        checkInput();
+    }
+});
+
+document.getElementById('reset-game').addEventListener("click", () => {
+    resetGame();
+    inputArea.focus();
+});
+
+document.getElementById('input-area').addEventListener('touchstart', ()=> {
+    if (!gameStarted) {
+        resetGame();
+        gameStarted = true;
+        startTimer();
+        checkInput();
+    } else {
+        checkInput();
+    }
+});
